453528c2007560cb180ad63713c9625681843bea
User <williampepple1@gmail.com>
2026-06-21 12:23:48 +0100

    Add export const dynamic = 'force-static' to dynamic image generators to support output export

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -5,6 +5,7 @@ export const size = {
   height: 180,
 };
 export const contentType = "image/png";
+export const dynamic = "force-static";
 
 export default function AppleIcon() {
   return new ImageResponse(
